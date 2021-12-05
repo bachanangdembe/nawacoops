1107d9fd09a7ec17eb9e31cf03e3ddf876e4f56e
bachanangdembe <91862195+bachanangdembe@users.noreply.github.com>
2021-12-05 11:22:17 +0545

    Delete template-parts directory

--- template-parts/footer/site-info.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Displays Site info
- */
-?>
-
-<div class="copyright">
-   <p><?php echo esc_html(get_theme_mod('solar_power_footer_copy',esc_html__('Solar Power WordPress By LogicalThemes','solar-power'))); ?></p>
-</div>
